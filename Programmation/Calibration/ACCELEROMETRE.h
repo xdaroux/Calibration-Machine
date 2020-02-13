25899e13c007f1478552e02f487b51fb17a6f7a2
Dartagnan Leonard <dartagnan.leonard@outlook.com>
2020-02-13 12:20:17 -0500

    Digital pin ACC
    - j'ai ajouter les digital pin 
    - condition pour savoir le nombre d<ACC connecter etc

--- Programmation/Calibration/ACCELEROMETRE.h
+++ Programmation/Calibration/ACCELEROMETRE.h
@@ -1,11 +1,18 @@
 #pragma once
 /*Pin Define  =================================================================*/
-#define ACC_0 A0
-#define ACC_1 A1
-#define ACC_2 A2
-#define ACC_3 A3
-#define ACC_4 A4
+#define pin_ACC_0 A0
+#define pin_ACC_1 A1
+#define pin_ACC_2 A2
+#define pin_ACC_3 A3
+#define pin_ACC_4 A4
 
+#define pin_ACC_0_CONNECTED  3
+#define pin_ACC_1_CONNECTED  4
+#define pin_ACC_2_CONNECTED  5
+#define pin_ACC_3_CONNECTED  6
+#define pin_ACC_4_CONNECTED  7
+
+enum{ACC_0,ACC_1,ACC_2,ACC_3,ACC_4};
 /*Define  =====================================================================*/
 #define GRAVITE 9.81
 #define ZERO_REF_ACC_0 520
@@ -17,7 +24,7 @@ typedef struct
 {
   uint16_t rawAcc;
   float gAcc;
-  uint8_t count;
+  int count;
 } T_ACC;
 
 /*Declaration des fonctions ===================================================*/
@@ -25,6 +32,7 @@ void ACC_init(T_ACC *);
 void ACC_init_tableau(T_ACC *, int dimension);
 void ACC_convertRawToG(T_ACC *acc, float zero_ref, float echelle);
 void ACC_read(T_ACC *acc, int analogPin, int count);
+void ACC_read_raw_acc(uint16_t *rawAcc, int analogPin, int count);
 void ACC_afficher(T_ACC *acc);
 
 /*Definition des fonctions  ===================================================*/
@@ -60,6 +68,11 @@ void ACC_read(T_ACC *acc, int analogPin, int count)
   acc->count = count;
 }
 
+void ACC_read_raw_acc(uint16_t *rawAcc, int analogPin, int count)
+{
+  *rawAcc = analogRead(analogPin);
+  
+}
 //afficher
 void ACC_afficher(T_ACC *acc)
 {
--- Programmation/Calibration/Calibration.ino
+++ Programmation/Calibration/Calibration.ino
@@ -1,7 +1,7 @@
 
 
 #include "Calibration.h"
-
+#define DEBUG 1
 // un calibration comporte juste un accelerometre donc on fait un tableau de calibration si on a 3 calibration a faire [3];
 //je vais m'arranger pour que sa le detecte au debut lors de l'alimentation quick connecte qui short une pin pour detecter !
 //et mettre le bon nombre dans lel tableau en foction de sa !
@@ -15,10 +15,13 @@ unsigned long timerStopInterupt;
 unsigned long timerSwitch = millis();
 
 #define pinSwitch 13
+#define NOMBRE_ACC_MAX 5
 
-uint8_t numberAcc = 4;    // va falloir que je fasse varier se chiffre en  fonction des input output 
-uint8_t testAccNum = 0;   // Savoir qu'elle accelerometre on test
+uint8_t numberAccConnected = 0; //Nombre d'accelerometre connecter
+uint8_t testAccNum = 0;         // Savoir qu'elle accelerometre on test
 
+int16_t pinAnalogReadSequence[NOMBRE_ACC_MAX]; // faut je les mettre tous a -1 si non
+int8_t pinAccConnected[NOMBRE_ACC_MAX];        // Etat des pin detection accelerometre peut oprimiser avec de bit faut init a 0
 void setup()
 {
   Serial.begin(9600);
@@ -28,34 +31,198 @@ void setup()
 
   pinMode(pinSwitch, INPUT_PULLUP);
 
+  pinMode(pin_ACC_0_CONNECTED, INPUT_PULLUP);
+  pinMode(pin_ACC_1_CONNECTED, INPUT_PULLUP);
+  pinMode(pin_ACC_2_CONNECTED, INPUT_PULLUP);
+  pinMode(pin_ACC_3_CONNECTED, INPUT_PULLUP);
+  pinMode(pin_ACC_4_CONNECTED, INPUT_PULLUP);
+
   //calibration
   Calibration_init(&CALIBRATION);
 
   //acceleration
   analogReference(EXTERNAL); // IMPORTANT 3.3V por cet accelerometre
   ACC_init_tableau(&ACC[0], DIMENSION);
+
+  /*
+  ici il va falloir faire une condition pour savoir combien acc/l/rometre
+   son connecter eb shortant un digtal pin quand le connecteur est mis
+   pour incrementer number Acc
+  */
+
+  //init tableau vite
+  pinAccConnected[ACC_0] = HIGH;
+  pinAccConnected[ACC_1] = HIGH;
+  pinAccConnected[ACC_2] = HIGH;
+  pinAccConnected[ACC_3] = HIGH;
+  pinAccConnected[ACC_4] = HIGH;
 }
 
 void loop()
 {
   //RPM calcul
+  int8_t tmp = 0;
   RPM_main(&RPM); // TJRS le faire, car utile pour tout le reste
   //calibration
 
   //Serial.println(CALIBRATION.etat);
-  if (millis() - timerSwitch > 250)
-  {
-    if (RPM.leRpm > 500)
-    {
-      if (digitalRead(pinSwitch) == 0)
-      {
-        CALIBRATION.etat = START;
-        timerSwitch = millis();
-      }
-    }
-  }
+
   switch (CALIBRATION.etat)
   {
+  case INIT:
+    if (DEBUG)
+    {
+      Serial.println("INIT");
+      delay(1000);
+      Serial.print(digitalRead(pin_ACC_0_CONNECTED));
+      Serial.print("  !=  ");
+      Serial.println(pinAccConnected[ACC_0]);
+      Serial.print(digitalRead(pin_ACC_1_CONNECTED));
+      Serial.print("  !=  ");
+      Serial.println(pinAccConnected[ACC_1]);
+      Serial.print(digitalRead(pin_ACC_2_CONNECTED));
+      Serial.print("  !=  ");
+      Serial.println(pinAccConnected[ACC_2]);
+      Serial.print(digitalRead(pin_ACC_3_CONNECTED));
+      Serial.print("  !=  ");
+      Serial.println(pinAccConnected[ACC_3]);
+      Serial.print(digitalRead(pin_ACC_4_CONNECTED));
+      Serial.print("  !=  ");
+      Serial.println(pinAccConnected[ACC_4]);
+    }
+    //Gestion nombre Acc connecter
+
+    // si un de ses condition est vrai, on refait le mapping
+
+    if (digitalRead(pin_ACC_0_CONNECTED) != pinAccConnected[ACC_0])
+    {
+      if (DEBUG)
+      {
+        Serial.println("Etat Acc 0 Modifier");
+      }
+      tmp = -1;
+    }
+    if (digitalRead(pin_ACC_1_CONNECTED) != pinAccConnected[ACC_1])
+    {
+      if (DEBUG)
+      {
+        Serial.println("Etat Acc 1 Modifier");
+      }
+      tmp = -1;
+    }
+    if (digitalRead(pin_ACC_2_CONNECTED) != pinAccConnected[ACC_2])
+    {
+      if (DEBUG)
+      {
+        Serial.println("Etat Acc 2 Modifier");
+      }
+      tmp = -1;
+    }
+    if (digitalRead(pin_ACC_3_CONNECTED) != pinAccConnected[ACC_3])
+    {
+      if (DEBUG)
+      {
+        Serial.println("Etat Acc 3 Modifier");
+      }
+      tmp = -1;
+    }
+    if (digitalRead(pin_ACC_4_CONNECTED) != pinAccConnected[ACC_4])
+    {
+      if (DEBUG)
+      {
+        Serial.println("Etat Acc 4 Modifier");
+      }
+      tmp = -1;
+    }
+
+    //mapping
+    if (DEBUG)
+    {
+      Serial.print("tmp : ");
+      Serial.println(tmp);
+    }
+
+    if (tmp == -1)
+    {
+      numberAccConnected = 0;
+
+      if (digitalRead(pin_ACC_0_CONNECTED) == LOW)
+      {
+        pinAnalogReadSequence[numberAccConnected] = pin_ACC_0;
+        numberAccConnected++;
+        pinAccConnected[ACC_0] = LOW;
+      }
+      else
+      {
+        pinAnalogReadSequence[numberAccConnected] = -1;
+        pinAccConnected[ACC_0] = HIGH;
+      }
+
+      if (digitalRead(pin_ACC_1_CONNECTED) == LOW)
+      {
+        pinAnalogReadSequence[numberAccConnected] = pin_ACC_1;
+        numberAccConnected++;
+        pinAccConnected[ACC_1] = LOW;
+      }
+      else
+      {
+        pinAnalogReadSequence[numberAccConnected] = -1;
+        pinAccConnected[ACC_1] = HIGH;
+      }
+
+      if (digitalRead(pin_ACC_2_CONNECTED) == LOW)
+      {
+        pinAnalogReadSequence[numberAccConnected] = pin_ACC_2;
+        numberAccConnected++;
+        pinAccConnected[ACC_2] = LOW;
+      }
+      else
+      {
+        pinAnalogReadSequence[numberAccConnected] = -1;
+        pinAccConnected[ACC_2] = HIGH;
+      }
+
+      if (digitalRead(pin_ACC_3_CONNECTED) == LOW)
+      {
+        pinAnalogReadSequence[numberAccConnected] = pin_ACC_3;
+        numberAccConnected++;
+        pinAccConnected[ACC_3] = LOW;
+      }
+      else
+      {
+        pinAnalogReadSequence[numberAccConnected] = -1;
+        pinAccConnected[ACC_3] = HIGH;
+      }
+
+      if (digitalRead(pin_ACC_4_CONNECTED) == LOW)
+      {
+        pinAnalogReadSequence[numberAccConnected] = pin_ACC_4;
+        numberAccConnected++;
+        pinAccConnected[ACC_4] = LOW;
+      }
+      else
+      {
+        pinAnalogReadSequence[numberAccConnected] = -1;
+        pinAccConnected[ACC_4] = HIGH;
+      }
+      Serial.print("Number ACC Connected : ");
+      Serial.println(numberAccConnected);
+      tmp = 0;
+    } // fin mapping
+
+    //Gestion du bouton test
+    if (millis() - timerSwitch > 250)
+    {
+      if (RPM.leRpm > 500)
+      {
+        if (digitalRead(pinSwitch) == 0)
+        {
+          CALIBRATION.etat = START;
+          timerSwitch = millis();
+        }
+      }
+    }
+    break;
   case START:
     Serial.println("START");
 
@@ -115,8 +282,9 @@ void loop()
     CALIBRATION.etat = CALCUL;
   }
   break;
-  case CALCUL:
+  case CALCUL: // calcul de la moyenne
     Serial.println("CALCUL");
+    testAccNum++; //Passer a la prochaine pin
     CALIBRATION.etat = INIT;
     break;
   case AFFICHER:
